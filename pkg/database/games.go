525b594cefb58fe61422bcf67436c2b5c498ce9c
Mike J. Renaker / "MikeDEV <mikejrenaker@proton.me>
2025-05-16 16:39:46 -0400

    Tweak GetGame function

--- pkg/database/games.go
+++ pkg/database/games.go
@@ -5,10 +5,14 @@ import (
 	"gorm.io/gorm"
 )
 
-func (d *Database) GetGame(id string) *types.DeveloperGame {
-	var game *types.DeveloperGame
-	if d.DB.Preload("Developer").Find(&game, "id = ?", id).Error == gorm.ErrRecordNotFound {
-		return nil
+func (d *Database) GetGame(id string) (game *types.DeveloperGame) {
+	res := d.DB.Preload("Developer").Where("id = ?", id).First(&game)
+	if res.Error != nil {
+		if res.Error == gorm.ErrRecordNotFound {
+			return nil
+		} else {
+			panic(res.Error)
+		}
 	}
 	return game
 }
